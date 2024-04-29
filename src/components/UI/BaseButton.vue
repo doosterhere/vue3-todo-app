331f4c29b17df6b23594e69e31bed400de1fc7cc
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-04-29 20:43:42 +0300

    Add BaseButton UI component

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import {defineComponent, type PropType} from 'vue'
+
+export default defineComponent({
+  name: "BaseButton",
+  props: {
+    type: {
+      type: String as PropType<'button' | 'submit' | 'reset'> | undefined,
+      default: 'button'
+    }
+  }
+})
+</script>
+
+<template>
+  <button
+      :type
+      class="base-button"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.base-button {
+  padding: 5px 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+
+  font: inherit;
+
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>

3ec900e63f194d444c5a0726de9729547d0c17fd
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-04-29 20:56:11 +0300

    Add BaseInput UI component

---
+++ src/components/UI/BaseInput.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BaseInput',
+  props: {
+    placeholder: {
+      type: String
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    modelValue: {
+      type: [String, Number]
+    }
+  }
+})
+</script>
+
+<template>
+  <input
+    :type
+    :placeholder
+    :value="modelValue"
+    class="base-input"
+  >
+</template>
+
+<style scoped lang="scss">
+.base-input {
+  width: 100%;
+  padding: 8px 12px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-sizing: border-box;
+
+  font-size: 1.6rem;
+  font-family: inherit;
+
+  outline: none;
+}
+</style>

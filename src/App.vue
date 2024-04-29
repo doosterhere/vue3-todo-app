74146b3b36974b6fc4e618e52805684f3aca386e
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-04-29 22:13:43 +0300

    Changing App component
    * Using AppHeader component;
    * Add async fetchTodos function;
    * Add computed properties: activeTodos, doneTodos, stats;

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,80 @@
-<script setup lang="ts">
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+import axios from "axios";
+
+import type {Todo} from "@/types/Todo";
+
+import AppHeader from "@/components/AppHeader.vue";
+
+interface State {
+  todos: Todo[],
+  page: number,
+  totalPages: number,
+  limit: number,
+  isTodoLoading: boolean
+}
+
+export default defineComponent({
+  name: "App",
+  components: {
+    AppHeader
+  },
+  data(): State {
+    return {
+      todos: [],
+      page: 1,
+      totalPages: 0,
+      limit: 10,
+      isTodoLoading: false
+    }
+  },
+  computed: {
+    activeTodos(): Todo[] {
+      return this.todos.filter(todo => !todo.completed);
+    },
+    doneTodos(): Todo[] {
+      return this.todos.filter(todo => todo.completed);
+    },
+    stats() {
+      return {
+        active: this.activeTodos.length,
+        done: this.doneTodos.length
+      }
+    }
+  },
+  methods: {
+    async fetchTodos(): Promise<void> {
+      try {
+        this.isTodoLoading = true;
+        const response = await axios('https://jsonplaceholder.typicode.com/todos', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+        this.todos = response.data;
+      } catch {
+        alert('Error while loading todos');
+      } finally {
+        this.isTodoLoading = false;
+      }
+    }
+  },
+  mounted() {
+    this.fetchTodos();
+  },
+});
 </script>
 
 <template>
-  <div></div>
+  <AppHeader
+      :stats
+  />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
 </style>

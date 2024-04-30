4b721f85895d22468a5d804f61cb6e54366d028a
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-04-30 13:32:56 +0300

    Adding the AppFooter component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppFooter'
+})
+</script>
+
+<template>
+  <footer class="footer">
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.footer {
+  height: 20px;
+}
+</style>

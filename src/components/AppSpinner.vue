69688b687c8cd9b6568e8fcfcaac561bbd5e7265
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-04-30 15:00:15 +0300

    Add the AppSpinner component

---
+++ src/components/AppSpinner.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AppSpinner"
+})
+</script>
+
+<template>
+  <div class="spinner">
+    <i class="fa-solid fa-spinner"></i>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/variables";
+
+.spinner {
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 2.5rem;
+  color: $color-primary;
+
+  animation: rotate 1s infinite linear;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>

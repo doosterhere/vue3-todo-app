74b0ae196f83cb161889acabbebb24caa5dead98
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-04-30 11:22:21 +0300

    Changing the main.ts
    Added global import for the BaseModal UI component;

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import './assets/global.scss';
 import App from './App.vue';
 import BaseButton from "@/components/UI/BaseButton.vue";
 import BaseInput from "@/components/UI/BaseInput.vue";
+import BaseModal from "@/components/UI/BaseModal.vue";
 
 const app = createApp(App);
 
 app
     .component('BaseButton', BaseButton)
-    .component('BaseInput', BaseInput);
+    .component('BaseInput', BaseInput)
+    .component('BaseModal', BaseModal);
 
 app.mount('#app');

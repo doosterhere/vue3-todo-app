cf14e8ce329c58a634e16b062838a8d99a4aa01b
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-05-07 11:22:18 +0300

    Changing the AppSpinner component
    * Tailwind styles have been used;
    * <style> section has been removed.

--- src/components/AppSpinner.vue
+++ src/components/AppSpinner.vue
@@ -7,30 +7,12 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="spinner">
+  <div
+      class="absolute -right-1/3 top-2
+      w-8 h-8 flex justify-center items-center
+      text-[2.5rem] text-primary
+      animate-spin"
+  >
     <i class="fa-solid fa-spinner"></i>
   </div>
 </template>
-
-<style scoped lang="scss">
-@import "@/assets/variables";
-
-.spinner {
-  width: 2rem;
-  height: 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  font-size: 2.5rem;
-  color: $color-primary;
-
-  animation: rotate 1s infinite linear;
-}
-
-@keyframes rotate {
-  100% {
-    transform: rotate(360deg);
-  }
-}
-</style>

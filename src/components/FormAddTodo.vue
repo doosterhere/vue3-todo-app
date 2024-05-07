7a3c22e0ce4b5dff6edca3c4714f7f8351fe2700
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-05-07 21:18:27 +0300

    Changing the FormAddTodo component
    The Fontawesome icons have been changed to SVG.

--- src/components/FormAddTodo.vue
+++ src/components/FormAddTodo.vue
@@ -49,11 +49,18 @@ export default defineComponent({
     />
     <div class="flex gap-6">
       <BaseButton @click="addTodo" class="flex-shrink flex-grow flex-auto">
-        <i class="fa-solid fa-check text-done"></i>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             class="w-8 h-8 text-done">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5"/>
+        </svg>
         Add todo
       </BaseButton>
       <BaseButton @click="cancelAddTodo" class="flex-shrink flex-grow flex-auto">
-        <i class="fa-solid fa-ban text-danger"></i>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             class="w-8 h-8 text-danger">
+          <path stroke-linecap="round" stroke-linejoin="round"
+                d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"/>
+        </svg>
         Cancel
       </BaseButton>
     </div>

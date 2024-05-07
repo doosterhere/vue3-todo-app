2025f2f21de5eb94045797a83ea3544a77591058
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-05-07 15:05:21 +0300

    Changing the BaseButton component
    * Applied tailwind styles;
    * <style> section has been removed.

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -15,40 +15,13 @@ export default defineComponent({
 <template>
   <button
       :type
-      class="base-button"
+      class="py-2 px-3.5 flex justify-center items-center gap-4 border border-lightgray rounded-lg
+      font-[inherit]
+      bg-transparent
+      cursor-pointer
+      active:scale-95
+      hover:font-medium"
   >
     <slot></slot>
   </button>
 </template>
-
-<style scoped lang="scss">
-@import '@/assets/variables.scss';
-
-.base-button {
-  padding: 5px 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-
-  font: inherit;
-
-  background-color: transparent;
-  cursor: pointer;
-
-  &.button_primary {
-    color: $color-primary;
-    border: 2px solid $color-primary;
-  }
-
-  &:active {
-    transform: scale(0.98);
-  }
-
-  &:not(.button_primary):hover {
-    font-weight: 500;
-  }
-}
-</style>

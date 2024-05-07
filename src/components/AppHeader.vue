07c4764d1fa769891a538e10621325736b78d921
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-05-07 21:29:07 +0300

    Changing the AppHeader component
    The Fontawesome icon has been changed to SVG.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -68,7 +68,10 @@ export default defineComponent({
     />
 
     <BaseButton @click="showAddTodoDialog">
-      <i class="fa-solid fa-plus text-primary"></i>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+           class="w-8 h-8 text-primary">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+      </svg>
       Add new todo
     </BaseButton>
   </header>
